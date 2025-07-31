3e761b83459f84e39600d60fd2a2baf0a5669be9
Kevin-Poli <tio22820@gmail.com>
2025-07-31 07:56:30 +0700

    merge with mansen

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-     protected $fillable = ['outlet_id', 'name', 'image', 'description', 'price', 'stock'];
+     protected $fillable = ['outlet_id', 'name', 'image', 'description', 'price', 'stock', 'description'];
 
     public function outlet()
     {
